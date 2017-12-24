6847101d983632af39b9f66de9e900a4d998b9d5
bablu9090 <34217558+bablu9090@users.noreply.github.com>
2017-12-23 23:32:48 -0500

    age changed

--- chapter05/J05_01.java
+++ chapter05/J05_01.java
@@ -17,16 +17,16 @@ public class J05_01 {
 		if(Age<=0)
 			System.out.println("You have not borned yet!");
 		
-		if(Age>0 && Age<=12)
+		if(Age>0 && Age<=11)
 			System.out.println("You are a child.");
 		
-		if(Age>12 && Age<=19)
+		if(Age>11 && Age<=18)
 			System.out.println("You are a teen ager.");
 		
-		if(Age>19 && Age<=40)
+		if(Age>18 && Age<=41)
 			System.out.println("You are young.");
 		
-		if(Age>40)
+		if(Age>41)
 			System.out.println("Wish your long life.");
 		
 		scnr.close();
