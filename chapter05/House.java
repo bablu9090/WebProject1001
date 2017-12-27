516e2716b658eb7f8ec0cba049badd9c2655b821
bablu9090 <34217558+bablu9090@users.noreply.github.com>
2017-12-26 20:14:26 -0500

    Add files via upload
    assn 1

---
+++ chapter05/House.java
@@ -0,0 +1,14 @@
+package assn001;
+
+public class House {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int size=20;
+if (size>=20);
+System.out.println("big house");    
+System.out.println("small house");
+	
+	}
+
+}
---
+++ chapter05/Student.java
@@ -0,0 +1,19 @@
+package assn001;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Class5=20;
+		int Class6=30;
+		int Class7=40;
+		if (Class5>Class6);
+		System.out.println("this is true");
+		if (Class5>Class6);
+			System.out.println("this is false");
+			if (Class7>Class5);
+			System.out.println("this is true");
+		}
+	}
+
+
